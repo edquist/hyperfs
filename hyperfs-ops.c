83474c4d2d7bc90c16588c2d11f7ded964b2afca
Carl Edquist <edquist@cs.wisc.edu>
2022-11-15 14:06:11 -0600

    WIP - get started with hyperfs-ops

---
+++ hyperfs-ops.c
@@ -0,0 +1,48 @@
+#define FUSE_USE_VERSION 26
+
+// #define _XOPEN_SOURCE 500
+// #define _GNU_SOURCE
+
+// #include <fuse.h>
+
+#include <stdio.h>     // FILE
+#include <errno.h>     // errno
+#include <time.h>      // time_t
+#include <sys/stat.h>  // struct stat
+
+#include "hyperfs-cache.h"
+
+
+static FILE *logger;
+
+#define LOG(...) do { if (logger) fprintf(logger, __VA_ARGS__); } while (0)
+
+
+// static
+int hyperfs_getattr(const char *path, struct stat *stbuf)
+{
+	int res;
+
+	struct ministat mst;
+	if (get_cached_path_info(path, &mst) < 0) {
+
+		// 1. fill mst properly, eg:
+		//    get_http_path_info(serverinfo, path, &mst);
+
+		// 2. Then:
+
+		set_cached_path_info(path, &mst);  // ignore failure
+	}
+	expand_ministat(&mst, stbuf);
+
+	LOG("[getattr: '%s']\n", path);
+	res = lstat(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+
+/* vim: set noexpandtab sts=0 sw=8 ts=8: */
+
