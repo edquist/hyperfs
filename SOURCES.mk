450973c6880de4f20d2157e767fa635ac6d8af43
Carl Edquist <edquist@cs.wisc.edu>
2022-11-16 06:10:25 -0600

    separate out common "library" sources

--- SOURCES.mk
+++ SOURCES.mk
@@ -1,6 +1,7 @@
 # SRCS.program = prog.cpp progsrc2.cpp progsrc3.cpp ...
 
-SRCS.getrange = getrange-main.c connor.c getrange.c cheddar.c \
-                ymdhms.c month_idx.c date_parse.c drainf.c \
-                hyperfs-cache.c hyperfs-ops.c
+LIB.SRCS      = connor.c getrange.c cheddar.c \
+                ymdhms.c month_idx.c date_parse.c drainf.c
+
+SRCS.getrange = getrange-main.c $(LIB.SRCS)
 
