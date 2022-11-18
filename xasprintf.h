48947bc70e2836ce57e62706e4cbb7b656bc90e5
Carl Edquist <edquist@cs.wisc.edu>
2022-11-18 07:37:27 -0600

    introduce xasprintf
    i'd like the idea of making this a proper function, but i do not feel
    like doing stdarg processing, so, a macro it is.
    
    note: i am generally thinking to use our own hyperfs-cache addpath
    mechanism for allocating strings (especially for strings which are
    expected to be valid until the end of the program) ... but i think
    this may be handy for a few items at startup.
    
    NOTE: _GNU_SOURCE should be defined at the top of any source file
    that uses xasprintf, before any headers are included (even this one).
    The xasprintf.h doesn't itself define _GNU_SOURCE, because i don't
    want to introduce any awkward rules about our headers having to be
    included in a particular order.

---
+++ xasprintf.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef _GNU_SOURCE
+#error Must define _GNU_SOURCE for asprintf/xasprintf
+#endif
+
+#include <stdio.h>    // asprintf
+
+#define FAIL(...) do { fprintf(stderr, __VA_ARGS__); exit(1); } while (0)
+
+// do this as a macro to ease passing __VA_ARGS__
+
+#define xasprintf(...) \
+	({ \
+		char *s; \
+		if (asprintf(&s, __VA_ARGS__) < 0) FAIL("asprintf alloc"); \
+		s; \
+	}) \
+
+
+/* vim: set noexpandtab sts=0 sw=8 ts=8: */
+
