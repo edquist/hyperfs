f00c6b3bc22010a1e1eefb78507562853d210f98
Carl Edquist <edquist@cs.wisc.edu>
2022-11-18 10:58:10 -0600

    add new pathbuf functions to hyperfs-cache.h

--- hyperfs-cache.h
+++ hyperfs-cache.h
@@ -9,6 +9,8 @@ int get_cached_path_info(const char *path, struct ministat *st);
 int set_cached_path_info(const char *path, const struct ministat *st);
 void expand_ministat(const struct ministat *mst, struct stat *st);
 char *addpath(const char *path);
+char *get_pathbuf(size_t size);
+char *add_pathbuf(const char *buf, size_t size);
 
 
 /* vim: set noexpandtab sts=0 sw=8 ts=8: */
