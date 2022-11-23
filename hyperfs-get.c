204282bc9e2da6717b719a22da935ffdfc6ef127
Carl Edquist <edquist@cs.wisc.edu>
2022-11-23 14:27:15 -0600

    introduce hyperget
    feels like getrange or get_head_info, but this time ask for the whole
    file (GET) and leave the stream ready to start reading it.
    
    feels a bit bloated, if i'm honest.  possibly with all the error
    handlng, idk.  might be nice to break it up or something.
    
    anyway, this will be used for reading the automatic index pages
    for /dir/ paths.

---
+++ hyperfs-get.c
@@ -0,0 +1,61 @@
+#include <stdio.h>   // fprintf, FILE, perror
+#include <unistd.h>  // close
+
+#include "hyperfs-state.h"  // struct hyperfs_state
+#include "connor.h"         // tcp_connect
+#include "cheddar.h"        // struct resp_info
+#include "logger.h"         // LOG
+
+
+FILE *hyperget(
+	struct hyperfs_state *remote,
+	const char           *path,
+	size_t               *content_len)
+{
+	LOG("[hyperget: '%s']\n", path);
+
+	int sock = tcp_connect(remote->host, remote->port);
+	if (sock < 0) {
+		perror("tcp_connect");
+		LOG("[hyperget: tcp_connect returned %d]\n", sock);
+		return NULL;
+	}
+
+	FILE *sockf = fdopen(sock, "r+");
+	if (!sockf) {
+		perror("fdopen");
+		LOG("[hyperget: fdopen failed]\n");
+		close(sock);
+		return NULL;
+	}
+
+	fprintf(sockf,  // XXX: check
+		"GET %s%s HTTP/1.1\r\n"
+		"Host: %s\r\n"  // skip port
+		// "Accept: */*\r\n"
+		// "User-Agent: hyperfs\r\n"
+		"\r\n", remote->rootpath, path, remote->host);
+
+	fflush(sockf);  // XXX: check
+
+	struct resp_info resp;
+        int ret = get_resp_info(sockf, &resp);
+	if (ret < 0) {
+		LOG("[hyperget: get_resp_info returned %d]\n", ret);
+		fclose(sockf);
+		return NULL;
+	}
+
+	LOG("[hyperget: got code %d]", resp.code);
+	if (resp.code < 200 || 299 < resp.code) {
+		fclose(sockf);
+		return NULL;
+	}
+
+	*content_len = resp.content_length;
+	return sockf;
+}
+
+
+/* vim: set noexpandtab sts=0 sw=8 ts=8: */
+
---
+++ hyperfs-get.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdio.h>  // FILE
+
+struct hyperfs_state;
+
+FILE *hyperget(
+	struct hyperfs_state *remote,
+	const char           *path,
+	size_t               *content_len);
+
+
+/* vim: set noexpandtab sts=0 sw=8 ts=8: */
+
